409acbdf6b16f907a719466fc502229da032c93f
@Sαɳƚ Sυɾαƚ <65957472+santsurat@users.noreply.github.com>
2023-04-08 21:30:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function dta () {
+    var b = "123";
+    function my () {
+       // console.log(b);
+      // var gaga = "mnbv";
+       console.log(b);
+       //return gaga;
+    }
+   return my;
+    
+};
+var kuchh = dta();
+
+console.log(kuchh);
+
+kuchh();
